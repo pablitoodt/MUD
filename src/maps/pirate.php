2d6087ae92046c05c1ba913134da0339da56601a
neyzar <lucas.narguet451@gmail.com>
2024-09-26 14:00:43 +0200

    ajout map modif main.php

---
+++ src/maps/pirate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Jugid\Staurie\Example\Maps;
+
+use Jugid\Staurie\Component\Map\Blueprint;
+use Jugid\Staurie\Game\Position\Position;
+use Jugid\Staurie\Example\Npcs\Cain;
+use Jugid\Staurie\Example\Items\Sword;
+use Jugid\Staurie\Example\Monsters\Bouftou;
+
+class Map01 extends Blueprint {
+
+    private Position $position;
+
+    public function __construct()
+    {
+        $this->position = new Position(2,4);
+    }
+
+    public function name() : string {
+        return 'Test map';
+    }
+
+    public function description() : string {
+        return 'Bienvenue au Pirate install toi et goute a nous doux breuvage ';
+    }
+
+    public function position() : Position {
+        return $this->position;
+    }
+
+    public function npcs() : array {
+        return [new Cain()];
+    }
+
+    public function items() : array {
+        return [new Sword()];
+    }
+
+    public function monsters() : array {
+        return [new Bouftou()];
+    }
+}
