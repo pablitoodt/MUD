05e2dc43f9988ce87945c12092c1531c2f00a57b
neyzar <lucas.narguet451@gmail.com>
2024-09-26 10:10:09 +0200

    ajout dossier

---
+++ src/main.php
@@ -0,0 +1,9 @@
+<?php
+use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Pyl\Mud\Space;
+require_once __DIR__.'/vendor/autoload.php'; //A REMPLACER
+$staurie = new Staurie('My game');
+$staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
+$staurie->run(); //LANCE LE JEU
