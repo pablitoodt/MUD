cb10dd151f9da37f9c6ab5967bd6267407cd0c72
pablitoodt <pablodeteba@icloud.com>
2024-09-26 16:39:34 +0200

    ajout maps

---
+++ src/Maps/Manufacture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Pyl\Mud\Maps;
+
+use Jugid\Staurie\Component\Map\Blueprint;
+use Jugid\Staurie\Game\Position\Position;
+use Pyl\Mud\Items\Doliprane;
+use Pyl\Mud\Npcs\Goat;
+
+class Manufacture extends Blueprint {
+
+    private Position $position;
+
+    public function __construct()
+    {
+        $this->position = new Position(1,0);
+    }
+
+    public function name() : string {
+        return "Manufacture map";
+    }
+
+    public function description() : string {
+        return "Au alexandra la cuite t'y attendra";
+    }
+
+    public function position() : Position {
+        return $this->position;
+    }
+
+    public function npcs() : array {
+        return [new Goat()];
+    }
+
+    public function items() : array {
+        return [new Doliprane()];
+    }
+
+    public function monsters() : array {
+        return [];
+    }
+}
